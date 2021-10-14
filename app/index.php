8647bc95b3b4dd1dc2dd05ead16a0f4f106cf148
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-14 11:39:54 +0200

    Exemple MVC amb Hello World

---
+++ app/index.php
@@ -0,0 +1,3 @@
+<?php
+
+$greeting = 'Hola mon!';
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<!--Separation of concerns -> Separar "coses diferents"/concerns en diferents fitxers o seccions -> components-->
-<!--MVC -> Model Vista Controlador -> FC-->
 <?php
-echo '<h1>Hola mon1!</h1>';
-?>
-</body>
-</html>
+
+require 'app/index.php';
+
+require 'resources/views/index.blade.php';
