f7e43d2678f13da37fa66a3fbcfe43577a7a6d94
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-14 11:11:08 +0200

    Primer codi PHP i HTML

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--Separation of concerns -> Separar "coses diferents"/concerns en diferents fitxers o seccions -> components-->
+<!--MVC -> Model Vista Controlador -> FC-->
+<?php
+echo '<h1>Hola mon1!</h1>';
+?>
+</body>
+</html>
