917714cf4458794049b15354390240f55c784fd2
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-14 12:09:47 +0200

    Exemple amb funcions, seguretat contra atacs XSS i paràmetres entrada GET

--- app/index.php
+++ app/index.php
@@ -1,3 +1,18 @@
 <?php
 
-$greeting = 'Hola mon!';
+// QUERY STRING
+//var_dump ($_GET['name']);
+//$name = 'Sergi';
+
+// API
+function greet() {
+    $name = htmlspecialchars($_GET['name']);
+    $surname = $_GET['surname'];
+
+    return "Hola $name $surname!";
+}
+
+$greeting = greet();
+
+
+//$greeting = 'Hola ' . $_GET['name'] . ' '. $_GET['name'] .  '!';
