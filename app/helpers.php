761e03357600db264624816a9c52e88a5d3bae34
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-18 16:03:31 +0200

    Afegida funció connectDB

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,14 @@ function dd($xivato)
     var_dump($xivato);
     die();
 }
+
+function connectDB($config) {  // Dependency Injection
+    try {
+        return new PDO(
+            $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+            $config['database']['user'],
+            $config['database']['password']);
+    } catch (\Exception $e) {
+        echo 'Error de connexió a la base de dades';
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,20 +1,12 @@
 <?php
 
+require 'config.php';
+
 require 'app/helpers.php';
 
 require 'app/Task.php';
 
-require 'config.php';
-
-//connect -> API nom connect, sense parametres entrada i tornara objecte $dbh
-try {
-    $dbh = new PDO(
-        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
-        $config['database']['user'],
-        $config['database']['password']);
-} catch (\Exception $e) {
-    echo 'Error de connexió a la base de dades';
-}
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * FROM tasks;');
 
