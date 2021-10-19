8842e26d2820154f31f69ab70d23f582f44b4429
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-19 14:13:23 +0200

    Versió 0.7

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-
 return [
     'database' => [
-        'user' => 'debian-sys-maint',
-        'password' => 'ri48e17r9cfJIqGu',
-        'databasetype' => 'mysql',
-        'host' => 'localhost',
-        'name' => 'phplaraveldevs',
+        'user' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'databasetype' => $_ENV['DB_CONNECTION'],
+        'host' => $_ENV['DB_HOST'],
+        'name' => $_ENV['DB_DATABASE'],
     ]
 ];
--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -1,11 +1,16 @@
 <?php
 
+use Dotenv\Dotenv;
 use Framework\App;
 use Framework\Database\Connection;
 use Framework\Database\Database;
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
 
 App::bind('config', require 'config.php');
 
 App::bind('database', new Database(
     Connection::make(App::get('config')['database'])
 ));
+
